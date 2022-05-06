dfccc1fc0c34a68d0bb9f7aa43fb5cd3b5311e7f
FABIAN OROZCO CHAVES <fabian.orozcochaves@ucr.ac.cr>
2022-05-05 23:19:49 -0600

    Authenticator: creo clase que autentica si un usuario existe y tiene permisos de escritura

---
+++ src/common/Authenticator.py
@@ -0,0 +1,59 @@
+# UCR/ECCI/PI_redesOper Equipo 7 raspado.
+
+import json
+
+route = "../server/json/users.json"
+
+## class Authenticator
+# class in charge of validating the entered username and password; in addition to verifying the permissions that it has, comparing through the file of valid users.
+class Authenticator:
+
+	## Constructor
+	# initializes the attribute that contains the data in json file
+	def __init__(self):
+		with open(route) as file:
+			self._usersJson = json.load(file)  # list of the data in json file
+		self._actualUser = None  # user that will be verified
+
+	## Compares the values given by parameter with the values of the json object
+	# @param username represents the key of the json object
+	# @param password represents the key of the json object
+	# return boolean True if the user is in the json object, otherwise returns False
+	def checkLog(self, username, password):
+		for user in self._usersJson['users']:
+			if user["username"] == username and user["password"] == password:
+				self._actualUser = user
+				return True
+			else:
+				self._actualUser = None
+		return False
+	
+	## Check if a user has write permissions
+	# return boolean true if the user has write permissions, otherwise returns False
+	def userCanWrite(self):
+		try:
+			return self._actualUser["canWrite"]
+		except:
+			return False
+
+# end Authenticator class
+
+# test code. To run: python Authenticator.py
+def main():
+	print(f"\n===start===")
+
+	authenticator = Authenticator()
+	
+	# enter 'q' to exit
+	while True:
+		username = input(f"\n> username: ")
+		if username == 'q' : break
+		password = input(f"> password: ")
+		if password == 'q' : break
+		print(f"{username} is {authenticator.checkLog(username, password)}")
+		print(f"{username} can write: {authenticator.userCanWrite()}")
+
+	print(f"\n===end===\n")
+
+if __name__ == "__main__":
+  	main()
