30f888e366b988c9f2e93cd223b20be28d231125
daniel.escobargiraldo <daniel.escobargiraldo@ucr.ac.cr.com>
2022-05-10 22:59:06 -0600

    moved and deleted old files

--- src/testing.py
+++ src/testing.py
@@ -1,8 +1,16 @@
-from Communicator import Communicator
+from SimulatorTcp import SimulatorTcp
 import socket
 
-socketA = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-ipAddress = '127.0.0.1'
-port = 8080
-test = Communicator(socketA)
-test.sendMessage("hola", (ipAddress, port))
+
+if(__name__ == '__main__'):
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ip = '127.0.0.1'
+    port = 8080
+    addresInfo = [ip, port]
+    communication = SimulatorTcp(serverSocket, ip, port)
+    communication.connect()
+    message = "\"type\":\"login\""
+    communication.sendTcpMessage(message)
+    message = communication.receiveTcpMessage()
+
+
