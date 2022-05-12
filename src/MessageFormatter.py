c1077b950af6089e96329b56787b4954209fb57a
FABIAN OROZCO CHAVES <fabian.orozcochaves@ucr.ac.cr>
2022-05-12 17:35:48 -0600

    MessageFormatter: termino la clase

--- src/MessageFormatter.py
+++ src/MessageFormatter.py
@@ -2,9 +2,6 @@
 
 # from Utilities import *
 
-from textwrap import indent
-
-
 class MessageFormatter:
 ## Class in charge of converting a data set into a message with json format
 
@@ -25,7 +22,7 @@ class MessageFormatter:
         return self.__sQts(key) + ":" + f"{value}" + var_comma + ""
 
     ## Creates the necessary format for the syn, agreed by all groups.
-    # Example: {"type”:”syn”,"seq”:x}
+    # Example: {"type":"syn","seq":x}
     # @param seq tcp message sequence number
     # @return string result with json format
     def formatSyn(self, seq):
@@ -37,7 +34,7 @@ class MessageFormatter:
         return format
 
     ## Creates the necessary format for the ack, agreed by all groups.
-    # Example: {"type”:”ack”,"ack”:x+1,”seq”:y}
+    # Example: {"type":"ack","ack":x+1,"seq":y}
     # @param seq tcp message sequence number
     # @param ack tcp message akc (knowledge)
     # @param Optional port represents the new port to client [from server]
@@ -58,26 +55,37 @@ class MessageFormatter:
         return format
 
     ## Creates the necessary format for the login, agreed by all groups.
-    # Example: {"seq":1,"type":"login","fin":true,"username":"u","password":"p"} 
+    # Example: {"seq":1,"type":"login","fin":true,"username":"u","password":"p"}
+    # {"seq":13,"type":"login","fin":true,"username":"user","password":"pass","validated":true, "canWrite":true}
     # @param seq tcp message sequence number
     # @param username string representing the username of client
     # @param password username string representing the password of client
+    # FROM SERVER:
+    # @param validated STRING Optional is "true" if the user is in the database, "false" in otherwise
+    # @param canWrite STRING Optional is "true" if the user can write, "false" in otherwise
     # @return string result with json format
-    def formatLogin(self, seq, username, password):
+    def formatLogin(self, seq, username, password, validated = None, canWrite = None):
         format = "{"
         format += self.jsonFormat("seq",        seq,                        ",")
-        format += self.jsonFormat("type",       self.__sQts("login"),         ",")
-        format += self.jsonFormat("fin",        self.__sQts("true"),          ",")
-        format += self.jsonFormat("username",   self.__sQts(username),   ",")
-        format += self.jsonFormat("password",   self.__sQts(password))
+        format += self.jsonFormat("type",       self.__sQts("login"),       ",")
+        format += self.jsonFormat("fin",        "true",                     ",")
+        format += self.jsonFormat("username",   self.__sQts(username),      ",")
+        
+        if (validated != None and canWrite != None):
+            format += self.jsonFormat("password",   self.__sQts(password),  ",")
+            format += self.jsonFormat("validated",  validated,              ",")
+            format += self.jsonFormat("canWrite",   canWrite)
+        else:
+            format += self.jsonFormat("password",   self.__sQts(password))
+
         format += "}"
 
         return format
 
     ## Creates the necessary format for writing request, agreed by all groups.
     # Example: 
-    # {"seq”:225,"type":"request","fin":true,"request":"write","operation":"2+4+5+8+5+7"}
-    # {"seq”:228,"type":"request","fin":true,"request":"write","result":31,"operation":"2+4+5+8+5+7"}
+    # {"seq":225,"type":"request","fin":true,"request":"write","operation":"2+4+5+8+5+7"}
+    # {"seq":228,"type":"request","fin":true,"request":"write","result":31,"operation":"2+4+5+8+5+7"}
     # @param seq tcp message sequence number
     # @param fin tcp flag that indicates if is the end of message
     # @param operation represents the mathematical operation entered by the user
@@ -100,8 +108,8 @@ class MessageFormatter:
 
     ## Creates the necessary format for read request, agreed by all groups.
     # Example: 
-    # {"seq”:226,"type":"request","fin":true,"request":"read","index":2}
-    # {"seq”:229,"type":"request","fin":true,"request":"read","index":"2",”error”:false,”result”:31,”operation”:"2+4+5+8+5+7"}
+    # {"seq":226,"type":"request","fin":true,"request":"read","index":2}
+    # {"seq":229,"type":"request","fin":true,"request":"read","index":"2","error":false,"result":31,"operation":"2+4+5+8+5+7"}
     # @param seq tcp message sequence number
     # @param fin tcp flag that indicates if is the end of message
     # @param index represents the position of the mathematical operation in the database
@@ -128,7 +136,31 @@ class MessageFormatter:
 
         return format
 
-# {"seq”:228,"type":"request","fin":true,"request":"write","result":31,"operation":"2+4+5+8+5+7"}
+    ## Creates the necessary format for the disconnection, agreed by all groups.
+    # Example: {"seq":8,"type":"disconnect"} 
+    # @param seq tcp message sequence number
+    # @return string result with json format
+    def formatDisconnect(self, seq):
+        format = "{"
+        format += self.jsonFormat("seq", seq, ",")
+        format += self.jsonFormat("type", self.__sQts("disconnect"))
+        format += "}"
+
+        return format
+
+    ## Creates the necessary format for the error, agreed by all groups.
+    # Example: {"seq":2,"type":"error","message":"example"}
+    # @param seq tcp message sequence number
+    # @param message error description 
+    # @return string result with json format
+    def formatError(self, seq, message):
+        format = "{"
+        format += self.jsonFormat("seq", seq, ",")
+        format += self.jsonFormat("type", self.__sQts("error"), ",")
+        format += self.jsonFormat("message", self.__sQts(message))
+        format += "}"
+
+        return format
 
 ####################################################################################
 
@@ -141,6 +173,7 @@ def main():
     print("ACK:\n" + msg_f.formatAck(1,2) + "\n")
     print("ACK-Port\n" + msg_f.formatAck(1,2,4040) + "\n")
     print("Login\n" + msg_f.formatLogin(1,"user","pass") + "\n")
+    print("Resp-Login\n" + msg_f.formatLogin(1,"user","pass","true","true") + "\n")
 
     print("Req-Write\n" + msg_f.formatRequestWrite(1,"true","1+2") + "\n")
     print("Req-Write[result]\n" + msg_f.formatRequestWrite(1,"true","1+2",3) + "\n")
@@ -148,6 +181,8 @@ def main():
     print("Req-Read[index]\n" + msg_f.formatRequestRead(1,"true",1) + "\n")
     print("Req-Read all\n" + msg_f.formatRequestRead(1,"true",-1) + "\n")
     print("Resp-Read \n" + msg_f.formatRequestRead(1,"true",2,"false",3) + "\n")
+    print("Disconnect \n" + msg_f.formatDisconnect(1) + "\n")
+    print("Error \n" + msg_f.formatError(1, "mensaje de error") + "\n")
 
 if __name__ == "__main__":
   	main()
