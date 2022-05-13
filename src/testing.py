c68b6c04ac48fe4155ef754af185f5f1c6422bbc
daniel.escobargiraldo <daniel.escobargiraldo@ucr.ac.cr.com>
2022-05-12 18:44:53 -0600

    testing simula un cliente que no existe

--- src/testing.py
+++ src/testing.py
@@ -1,21 +1,22 @@
 from SimulatorTcp import SimulatorTcp
 import socket
 from Utilities import *
-
+from MessageFormatter import MessageFormatter
 
 if(__name__ == '__main__'):
     #====================================== client to server testing
-
+    format = MessageFormatter()
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     ip = '127.0.0.2'
     port = 8080
     addresInfo = [ip, port]
     communication = SimulatorTcp(serverSocket, ip, port)
     if (communication.connect()):
-        message = "\"type\":\"login\""
+        message = format.formatLogin("xd", "passbasic")
 
         communication.sendTcpMessage(message)
 
-        message = "\"type\":\"logout\""
+        #message = "\"type\":\"logout\""
 
-        communication.sendTcpMessage(message)
+        message = communication.receiveTcpMessage()
+        printMsgTime(f"Received the login confirmation: {message}")
