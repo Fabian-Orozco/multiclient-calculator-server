bacc97285c7fc8b616346f0f077535ad8f0eebb1
FABIAN OROZCO CHAVES <fabian.orozcochaves@ucr.ac.cr>
2022-05-12 21:38:36 -0600

    Client: implemento y agrego métodos de comunicación con el server

--- src/Client.py
+++ src/Client.py
@@ -2,9 +2,12 @@
 
 from Utilities import *
 from Args_analizer import Args_analizer
-# from SimulatorTcp import SimulatorTcp
+from SimulatorTcp import SimulatorTcp
 # from DataValidator import DataValidator
-
+from MessageFormatter import MessageFormatter
+import json
+import socket
+	
 
 ## class Client
 # class in charge of handling the interaction with the user
@@ -12,13 +15,15 @@ class Client:
 
 	## Constructor
 	# initializes the attributes
-	def __init__(self):
+	def __init__(self, port, host):
 		self.__argsAnalizer = Args_analizer()
-		# self.__comm = SimulatorTcp()
+		self.__comm = SimulatorTcp(socket.socket(socket.AF_INET, socket.SOCK_DGRAM), port, host)
 		# self.__validator = DataValidator()
+		self.__msgFormatter = MessageFormatter()
 		self.__username = ""
 		self.__password = ""
-
+		self.__canWrite = False
+	
 ####################################################################################
 
 	## method that calls the class that analyzes arguments to analyze if their format is valid or not
@@ -29,7 +34,7 @@ class Client:
 		else:
 			print(f"The format of the arguments {TXT_RED}is invalid{TXT_RESET}")
 			self.__argsAnalizer.printUsage()
-			self.close()
+			self.__close()
 			
 ####################################################################################
 	
@@ -37,16 +42,21 @@ class Client:
 	def run(self):
 		printMsgTime(f"{TXT_GREEN}|======: START :======|{TXT_RESET}")
 
-		self.__analizeArgs()  # if arguments are invalid invokes to close.
+		self.__analizeArgs()  # if arguments are invalid invokes to __close.
 		# chequea que sea un usuario registrado
-		if(self.login() == False): # invoca a tcp
-			self.close() 
-		
+
+		self.__connect()  # invoca a tcp. Si no es válido, cierra el programa
+		self.login()  # invoca a tcp. Si no es válido, cierra el programa
+
 		# enter 'q' to exit
 		while True:
 			input = self.showInterface()
-			if input == 'q' : self.close()
+			if input == 'q' : self.__close()
 			if input == '-h' : self.__argsAnalizer.printHelp()
+			# self.__validateData(input)
+			formatedMessage = self.__generateAction(input)
+			# self.__msgFormatter.
+			# self.__sendMessage(input)
 		
 
 ####################################################################################
@@ -62,15 +72,56 @@ class Client:
 		except EOFError as err:  # EOF (ctrl+d)
 			print(f' \n{TXT_RED}Program finished by signal EOF{TXT_RESET}')
 		return 'q'
-		
+####################################################################################
+
+	## prints the base message to the user
+	# @return the value entered by the user
+	def __checkInput(self, userInput):
+		if ("-r" in userInput):
+			return "read"
+		elif ("-w" in userInput):
+			return "write"
+		else:
+			return False
 
 ####################################################################################
 
 	##
 	#
-	def validateData(self):
+	def __validateData(self, userInput):
 		## El atributo de tipo DataValidator se encarga de esto ......Agregar.......
 		print("")
+####################################################################################
+
+	##
+	#
+	def __generateAction(self, userInput):
+		action = self.__checkInput(userInput)
+		userInput = userInput.strip()
+		if (action == "read"):
+			if (" " in userInput):
+				userInput = (userInput[userInput.find(" "):]).strip()
+				# Tcp se encarga de dividir. Se asumen "fin" como true
+				return self.__msgFormatter.formatRequestRead("true", int(userInput))
+
+			else: # solo está el "-r"
+				# Tcp se encarga de dividir. Se asumen "fin" como true
+				return self.__msgFormatter.formatRequestRead("true", -1)
+				
+		elif(action == "write"):
+			print("WRITE")
+		else:
+			print("INVALID ACTION")
+
+
+
+####################################################################################
+
+	##
+	#
+	def __connect(self):
+		if(self.__comm.connect() == False):
+			self.__close()
 
 ####################################################################################
 
@@ -78,21 +129,44 @@ class Client:
 	#
 	def login(self):
 		# Invokes messageFormatter to format the string (user+password) ..Agregar..
-		# Invokes Simulator_Tcp to send a message ........Agregar.........
-		print("")
+		message = self.__msgFormatter.formatLogin(self.__username, self.__password)
+		self.__sendMessage(message)
+		
+		response = self.__receiveMessage()
+		json_response = json.loads(response)
+
+		validated = json_response['validated']
+
+		if (validated == False):
+			printErrors("User not validated")
+			self.__close()
+		
+		printMsgTime(f"{TXT_GREEN}User validated.{TXT_RESET} Welcome {self.__username}\n")
+		self.__canWrite = json_response['canWrite']
+		return True
+		
 
 ####################################################################################
 
 	##
 	#
-	def receiveMessage(self):
-		# Invokes Simulator_Tcp to receive a message ........Agregar.........
-		print("")
+	def __sendMessage(self, message):
+		# Invokes Simulator_Tcp to send a message
+		self.__comm.sendTcpMessage(message)
 
 ####################################################################################
 
-	def close(self):
-		# Invokes Simulator_Tcp to close the connection ........Agregar.........
+	##
+	#
+	def __receiveMessage(self):
+		# Invokes Simulator_Tcp to receive a message ........Agregar.........
+		message = self.__comm.receiveTcpMessage()
+		return message
+
+####################################################################################
+
+	def __close(self):
+		# Invokes Simulator_Tcp to __close the connection ........Agregar.........
 		printMsgTime(f"{TXT_RED}|======: FINISH :======|{TXT_RESET}")
 		exit(0)
 
@@ -104,7 +178,7 @@ class Client:
 # test code. To run: python Client.py
 def main():
 
-	client = Client()
+	client = Client('127.0.0.2', 8080)
 	client.run()
 
 if __name__ == "__main__":
