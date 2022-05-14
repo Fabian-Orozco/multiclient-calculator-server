eac5456d2fb141aaf04eeb9ab6c0f8bdd604a8a1
daniel.escobargiraldo <daniel.escobargiraldo@ucr.ac.cr.com>
2022-05-13 22:35:35 -0600

    Server recibe requests del cliente, hace login, se deconecta y pone en cola las operaciones

--- src/Server.py
+++ src/Server.py
@@ -1,3 +1,4 @@
+from email import message
 from SimulatorTcp import SimulatorTcp
 import socket
 import threading
@@ -6,6 +7,7 @@ from Utilities import *
 from Authenticator import Authenticator
 import json
 from MessageFormatter import MessageFormatter
+import queue
 
 class Server:
 	def __init__(self, host, port):
@@ -17,13 +19,14 @@ class Server:
 		self.tcpCommunication = SimulatorTcp(self.__welcomingSocket, self.__host, self.__port)
 		self.__authenticator = Authenticator()
 		self.__formatter = MessageFormatter()
+		self.__requestsQueue = queue.LifoQueue()
 
 	def shutDownServer(self):
 		printMsgTime(f'{TXT_RED}|====Shutting down server====|{TXT_RESET}')
 		# shutdown the server closes the socket
 		self.__welcomingSocket.close()
 
-	def waitForClient(self):
+	def __waitForClient(self):
 		newPort = self.__port
 		# waits for client connection
 		try:
@@ -32,7 +35,7 @@ class Server:
 					newPort += 1
 					if (self.tcpCommunication.listen(newPort)):
 						# creates thread to manage the connection
-						thread = threading.Thread(target = self.handleConnection,
+						thread = threading.Thread(target = self.__handleConnection,
 							args = (newPort, self.tcpCommunication.getSeqAck(), \
 							self.tcpCommunication.getDestination()))
 						thread.daemon = True
@@ -40,21 +43,50 @@ class Server:
 				except KeyboardInterrupt or OSError or EOFError:
 					break
 		finally:
+			printMsgTime(f"{TXT_YELLOW}Server queue contains the next messages:{TXT_RESET}")
+			while not self.__requestsQueue.empty():
+				print(self.__requestsQueue.get())
 			self.shutDownServer()
 
-	def handleConnection(self, port, values, destination):
+	def __handleConnection(self, port, values, destination):
 		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		sock.bind((self.__host, port))
 		communicator = SimulatorTcp(sock, destination[0], destination[1])
 		# setting values to continue communication
 		communicator.setSeqAck(values)
 
-		if(self.clientLogin(communicator) == False):
+		loginAccepted, user = self.__clientLogin(communicator)
+		if(loginAccepted == False):
 			communicator.close()
 			return
+		action = self.__handleRequest(communicator)
+		if (action == "timeout"):
+			printMsgTime(f"User \"{user}\" {TXT_RED}timedout thread: {threading.get_ident()}{TXT_RESET}")
+		if (action == "disconnect"):
+			printMsgTime(f"User \"{user}\" {TXT_RED}disconnected thread: {threading.get_ident()}{TXT_RESET}")
 
 
-	def clientLogin(self, communicator):
+
+	def __handleRequest(self, communicator):
+		communicator.setTimeout(self.__maxTimeOut)
+		printMsgTime(f"Handling request in thread: {threading.get_ident()}")
+		messageReceived = communicator.receiveTcpMessage()
+		if (messageReceived == "timeout"):
+			return messageReceived
+		jsonMessage = json.loads(messageReceived)
+
+
+		while(jsonMessage["type"] != "disconnect"):
+			self.__requestsQueue.put(messageReceived)
+			communicator.setTimeout(self.__maxTimeOut)
+			messageReceived = communicator.receiveTcpMessage()
+			if (messageReceived == "timeout"):
+				return messageReceived
+			jsonMessage = json.loads(messageReceived)
+		return "disconnect"
+
+
+	def __clientLogin(self, communicator):
 		canWrite = False
 		userAccepted = False
 		message = communicator.receiveTcpMessage()
@@ -64,21 +96,20 @@ class Server:
 			canWrite = self.__authenticator.userCanWrite()
 
 		message = self.__formatter.formatLogin(loginInfo['username'], loginInfo['password'],  str(userAccepted).lower(), str(canWrite).lower())
-
+		user = loginInfo['username']
 		communicator.sendTcpMessage(message)
 		if (userAccepted):
 			printMsgTime(f"User \"{loginInfo['username']}\" {TXT_GREEN}accepted{TXT_RESET}")
-			return True
+			return (True, user)
 		else:
 			printMsgTime(f"User \"{loginInfo['username']}\" {TXT_RED}not accepted{TXT_RESET}")
-			
-			return False
+			return (False, user)
 
 	def run(self):
 		printMsgTime(f"{TXT_GREEN}|====Server started====|{TXT_RESET}")
 		printMsgTime(f"{TXT_YELLOW}Binded to ip:{self.__host} | port:{self.__port}{TXT_RESET}")
 
-		self.waitForClient()
+		self.__waitForClient()
 
 if(__name__ == '__main__'):
 	server = Server('127.0.0.2', 8080)
