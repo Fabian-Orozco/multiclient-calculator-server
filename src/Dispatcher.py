045b2d37a70702f6752cbde6d2a3593fceba4b8a
FABIAN OROZCO CHAVES <fabian.orozcochaves@ucr.ac.cr>
2022-05-15 23:15:29 -0600

    Dispatcher: modifico print

--- src/Dispatcher.py
+++ src/Dispatcher.py
@@ -15,6 +15,7 @@ class Dispatcher:
 		self.__communicator = SimulatorTcp(self.__sock, self.__host, self.__port)
 
 	def dispatch(self, message):
-		printMsgTime(f"{TXT_GREEN}Process B{TXT_RESET} will dispatch the next message: {message}")
+
+		printMsgTime(f"{TXT_GREEN}Process B{TXT_RESET} will dispatch the next message: {message.decode('utf-8')}")
 		# dispatcher just sends tne consumed messages of the queue to another server for now
 		# self.__communicator.sendTcpMessage(message)
