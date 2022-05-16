3709b5fce2f562db1b9da179a1c3fc3077413c47
daniel.escobargiraldo <daniel.escobargiraldo@ucr.ac.cr.com>
2022-05-15 21:17:46 -0600

    ifnoring testing

--- src/Dispatcher.py
+++ src/Dispatcher.py
@@ -6,11 +6,13 @@ import socket
 
 class Dispatcher:
 	def __init__(self, host, port):
+		# destination of the calculation server
 		self.__host = host            # server port
 		self.__port = port            # server ip
-		#self.__sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		#self.communicator = SimulatorTcp(self.__sock, self.__host, self.__port)
-		# self.communicator
+
+		# creation of TCP to dispatch operations to next layer
+		self.__sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.communicator = SimulatorTcp(self.__sock, self.__host, self.__port)
 
 	def dispatch(self, message):
 		printMsgTime(f"{TXT_GREEN}Process B received: {message}{TXT_RESET}")
