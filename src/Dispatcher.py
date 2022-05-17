beb52613a75d77762dbaf95ae601564283c1298f
FABIAN OROZCO CHAVES <fabian.orozcochaves@ucr.ac.cr>
2022-05-17 13:27:46 -0600

    Dispatcher: restauro dispatcher a version 1 del pipe

--- src/Dispatcher.py
+++ src/Dispatcher.py
@@ -1,4 +1,3 @@
-import ctypes
 from SimulatorTcp import SimulatorTcp
 from Utilities import *
 import queue
@@ -15,18 +14,8 @@ class Dispatcher:
 		self.__sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.__communicator = SimulatorTcp(self.__sock, self.__host, self.__port)
 
-	def dispatch(self):
-		libPipe = ctypes.CDLL('./libPipe.so')
-		libPipe.receiveMsg.restype = ctypes.c_char_p
-		try:
-			while(True):
-				message = libPipe.receiveMsg()
-				if(message == b"stop"):
-					print("Cerrando proceso B")
-					exit(0)
-				printMsgTime(f"{TXT_GREEN}Process B{TXT_RESET} will dispatch the next message: {message.decode('utf-8')}")
-		except KeyboardInterrupt:
-			exit(0)
-			
+	def dispatch(self, message):
+
+		printMsgTime(f"{TXT_GREEN}Process B{TXT_RESET} will dispatch the next message: {message.decode('utf-8')}")
 		# dispatcher just sends tne consumed messages of the queue to another server for now
 		# self.__communicator.sendTcpMessage(message)
