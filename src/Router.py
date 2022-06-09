834b144e74f7964ab9a7aa912d4844fdf6bb56ed
DanielEscobar19 <danielonchio@gmail.com>
2022-06-08 20:52:44 -0600

    Cargar sockets Router: Estrutura de sockets funcionando

--- src/Router.py
+++ src/Router.py
@@ -1,12 +1,16 @@
 import socket
 import threading
 import os
+
+from numpy import rint
 from Utilities import *
 import json
 from MessageFormatter import MessageFormatter
 import queue
 import sys
 from time import sleep
+import csv
+
 
 
 class Router:
@@ -14,46 +18,79 @@ class Router:
 		self.__serverIP = ip
 		self.__serverPort = port
 		self.__routerID = id
-		self.__SocketServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.__connectToServer()
+		self.__queuesMaxSize = 10
+		self.__socketServer = self.SocketStruct(ip, port, self.__queuesMaxSize, "Server")
+		self.__ownIp = socket.gethostbyname(socket.gethostname())
 	
 	def __connectToServer(self):
 		# router connects to server only if it received the ip and port from the console arguments
-		if (self.__serverIP != "-" or self.__serverPort != "-"):
-			printMsgTime(f"{TXT_YELLOW}Connecting to server{TXT_RESET} ip:{self.__serverIP} | port:{self.__serverPort}")
-			# tries to connect with server
-			while(True):
-				try:
-					self.__SocketServer.connect((self.__serverIP, self.__serverPort))
-					break
-				except KeyboardInterrupt:
-					self.__shutDown()
-				except:
-					printErrors("Server not found. Retrying connection.")
-					sleep(5)
-			self.__sendMsg(self.__SocketServer, "{\"type\":\"router\",\"id\":\""+ self.__routerID + "\"}")
-	
-	def __recvMsg(self, sock, timeout):
-		# maximum timeout is 5 minutes
-		sock.settimeout(timeout)
-		message = ""
-		try:
-			message = sock.recv(4096)
-			printMsgTime(f"{TXT_RED}Testing{TXT_RESET} received: {message}")
-			return message.decode('UTF-8')
-		except socket.timeout:
-			# timout reached
-			return "timeout"
+		if (self.__socketServer.connect() == False):
+			self.__shutDown()
+		self.__socketServer.sendMsg("{\"type\":\"router\",\"id\":\""+ self.__routerID + "\"}")
 
-	def __sendMsg(self, sock, message):
-		printMsgTime(f"{TXT_RED}Testing{TXT_RESET} sent: {message}")
-		sock.send(message.encode('UTF-8'))
+	def __createSockets(self, fileName):
+		x = 0
+
+
+	def __loadOwnSocket(self, port):
+		x = 0
+
+	def __loadNeighbordsSocket():
+		x = 0
 
 	def __shutDown(self):
 		printMsgTime(f'{TXT_RED}|======: Shutting down Router \"{self.__routerID}\":======|{TXT_RESET}')
 		# shutdown the server closes the socket
-		self.__SocketServer.close()
+		self.__socketServer.sock.close()
 		exit(0)
 
-	def run():
-		printMsgTime("Router running")
+	def run(self):
+		printMsgTime(f"{TXT_GREEN}|======: Router {self.__routerID} started :======|{TXT_RESET}")
+		self.__connectToServer()
+
+	class SocketStruct:
+		def __init__(self, otherIp, otherPort, maxSize, nId = "-", connectionWeight = 0 ):
+			self.neighbordId = nId
+			self.neighbordIp = otherIp
+			self.neighbordPort = otherPort
+			self.weight = connectionWeight
+			self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.inQueue = queue.Queue(maxSize)
+			self.outQueue = queue.Queue(maxSize)
+		
+		def bind(self, ip, port):
+			self.sock.bind((ip,port))
+
+		def sendMsg(self, message):
+			printMsgTime(f"{TXT_RED}Testing{TXT_RESET} sent: {message}")
+			self.sock.send(message.encode('UTF-8'))
+
+		def recvMsg(self, timeout):
+			# maximum timeout is 5 minutes
+			self.sock.settimeout(timeout)
+			message = ""
+			try:
+				message = self.sock.recv(4096)
+				printMsgTime(f"{TXT_RED}Testing{TXT_RESET} received: {message}")
+				return message.decode('UTF-8')
+			except socket.timeout:
+				# timout reached
+				return "timeout"
+
+		def connect(self):
+			if (self.neighbordIp != "-" or self.neighbordPort != "-"):
+				printMsgTime(f"{TXT_YELLOW}Connecting to {self.neighbordId}.{TXT_RESET} ip:{self.neighbordIp} | port:{self.neighbordPort}")
+				# tries to connect with server
+				while(True):
+					try:
+						self.sock.connect((self.neighbordIp, self.neighbordPort))
+						break
+					except socket.error:
+						printErrors(f"{self.neighbordId} not found. Retrying connection.")
+						sleep(5)
+				printMsgTime(f"{TXT_GREEN}Connection established to {self.neighbordId}.{TXT_RESET} ip:{self.neighbordIp} | port:{self.neighbordPort}")
+def main():
+	print(socket.gethostbyname(socket.gethostname()))
+
+if (__name__ == '__main__'):
+	main()
