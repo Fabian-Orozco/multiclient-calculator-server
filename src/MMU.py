0d9f79b2f1bd708842553f100f2c38f5aeee263d
Marco <marcoandres558@gmail.com>
2022-06-12 15:23:55 -0600

    Metosos saveRam,updatePagetable,updatePagedDisk: Realizado

--- src/MMU.py
+++ src/MMU.py
@@ -1,3 +1,4 @@
+
 KEY_TO_FRAME = 1
 CELDAS_EN_RAM = 4
 class MMU:
@@ -19,10 +20,27 @@ class MMU:
             pageFrames.append(diskOperation[char:char+4])
         page = {"index":index,"operation":diskOperation,"qtypags":len(pageFrames),"parts" : pageFrames}
         self._pagedDisk.append(page)
-        
+    
+    def addPage(self, pageFrames):
+        for pag in range(pageFrames["qtypags"]):
+            page = [pageFrames["index"], False, pag, False, -1]
+            self._pageTable.append(page)
+    
+    def saveRAM(self, operation, frame):
+        operation = bytes(operation, encoding="utf-8")
+        char = 0
+        for byt in range(frame, frame+4):
+            self._ram[byt] = operation[char]
+            char += 1
+        print(self._ram)
+
 
     #def _translate(self, page):
         
     #def _getOperation(self, operationNumber):
         
     #def _getPage(self, operationPart):
+
+if(__name__ == '__main__'):
+    mmu = MMU()
+    mmu.saveRAM("4+33",4)
