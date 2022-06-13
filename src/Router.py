3fb8466cf6c041345aec28f576ada9a55f8d1872
DanielEscobar19 <danielonchio@gmail.com>
2022-06-12 23:49:45 -0600

    Paso de mensajes router: Falta probar el paso de mensjaes entyre routers. Correcciones de errores simples y se corrigio la conexion entre el servidor y el cliente

--- src/Router.py
+++ src/Router.py
@@ -1,3 +1,4 @@
+from http import server
 import socket
 import threading
 import os
@@ -15,7 +16,6 @@ class Router:
 	def __init__(self, serverIp, serverPort, id):
 		# this router info
 		self.__routerID = id
-		self.__queuesMaxSize = 10
 		self.__ownIp = socket.gethostbyname(socket.gethostname())
 
 		# connection to server info
@@ -24,51 +24,78 @@ class Router:
 			self.__serverPort = int(serverPort) # maybe this will deleted
 		except:
 			self.__serverPort = serverPort
-		self.__socketServer = self.SocketStruct(self.__ownIp, None, serverIp, serverPort, "server")
+		# self.__socketServer = self.SocketStruct(self.__ownIp, None, serverIp, serverPort, "server")
+		
+		self.__socketServer = self.SocketStruct("127.0.0.1", None, serverIp, serverPort, "server") # TESTING
 
 		# Info to connect to other routers
 		self.__connections = dict({"server":self.__socketServer})
 		self.__connectionsFile = "neighbors.csv"
-	
+		self.__availableNodes = []
+		self.__topologyFile = "topologia.csv"
+
 		# routing table info
 		self.__routingTable = {}
-		self.__tableUpdatesQueue = queue.Queue()
 
 		# thread management
 		self.threadsArray = []
-	
+
+	def __loadAvailableNode(self):
+		with open(self.__topologyFile) as topology:
+			file = csv.reader(topology)
+			for row in file:  # each row of the file
+				if (row[0] != self.__routerID and row[0] not in self.__availableNodes):
+					self.__availableNodes.append(row[0])
+				if (row[3] != self.__routerID and row[3] not in self.__availableNodes):
+					self.__availableNodes.append(row[3])
+
 	def __connectToServer(self):
 		# router connects to server only if it received the ip and port from the console arguments
 		self.__socketServer.connect()
 		if (self.__serverIP != '-' or self.__serverPort != '-'):
 			self.__socketServer.sendMsg("{\"type\":\"router\",\"id\":\""+ self.__routerID + "\"}")
 
+	def __runConnectionToServer(self, sockStruct):
+		printMsgTime(f"{TXT_RED}Testing{TXT_RESET} Thread handles conection with {sockStruct.neighbordId}")
+		while(True):
+			try:
+				self.__receiveOperations(sockStruct, sockStruct)
+				
+				stopCondition = self.__processMessages(sockStruct)
+				if (stopCondition == "threadStop"):
+					return
+				if (sockStruct.outQueue.empty() == False):
+					self.__resendMessages(sockStruct)
+			except KeyboardInterrupt:
+				self.__shutDown()
+
 	def __createSockets(self, fileName):
-		with open(fileName) as topology:
-			file = csv.reader(topology)
+		with open(fileName) as neighbords:
+			file = csv.reader(neighbords)
 			for row in file:  # each row of the file
 				if (row[0] == self.__routerID):
 					newSocket = self.SocketStruct(self.__ownIp, row[1], row[3], row[4], row[2], row[5])
 					self.__connections[row[2]] = newSocket
 
-
-	def tableUpdater(self):
-		x=0
-
 	def __createthreads(self):
+
 		printMsgTime(f"{TXT_RED}Testing{TXT_RESET} Creating threads")
 		connectionsList = self.__connections.values()
-		for connection in connectionsList:
-			thread = threading.Thread(target = self.__runConnection, args = (connection,))
-			self.threadsArray.append(thread)
-			# deamon thread so it destoys itself when it has finished working
-			thread.daemon = True
+		try:
+			for connection in connectionsList:
+				if (connection.neighbordId != "server"):
+						thread = threading.Thread(target = self.__runConnection, args = (connection,))
+						self.threadsArray.append(thread)
+						# deamon thread so it destoys itself when it has finished working
+						thread.daemon = True
 
-			# thread starts to work
-			thread.start()
+						# thread starts to work
+						thread.start()
+		except KeyboardInterrupt or OSError or EOFError:
+			pass
+
 
 	def __runConnection(self, sockStruct):
-		x = 0
 		printMsgTime(f"{TXT_RED}Testing{TXT_RESET} Thread handles conection with {sockStruct.neighbordId}")
 		while(True):
 			sockStruct.bind()
@@ -80,7 +107,9 @@ class Router:
 				self.__receiveOperations(sockStruct)
 				newSockStruct.close()
 
-			self.__processMessages(sockStruct)
+			stopCondition = self.__processMessages(sockStruct)
+			if (stopCondition == "threadStop"):
+				return
 
 			sockStruct.close()
 			sockStruct.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -96,12 +125,16 @@ class Router:
 			operation = receiver.recvMsg()
 			if (operation == "timeout"):
 				return
-			sockStruct.inQueue.append(operation)
+			sockStruct.inQueue.put(operation)
 
 	def __processMessages(self, sockStruct):
-		printMsgTime(f"{TXT_RED}Testing{TXT_RESET} connection with router {sockStruct.neighbordId} is processing messages")
+		# printMsgTime(f"{TXT_RED}Testing{TXT_RESET} connection with router {sockStruct.neighbordId} is processing messages")
 		while (not sockStruct.inQueue.empty()):
-			message = sockStruct.inQueue.pop(block=False)
+			message = sockStruct.inQueue.get(block=False)
+
+			if (message == "threadStop"):
+				return "threadStop"
+
 			printMsgTime(f"{TXT_RED}Testing{TXT_RESET} connection with router {sockStruct.neighbordId} is processing: {message}")
 			jsonMessage = json.loads(message)
 
@@ -117,34 +150,52 @@ class Router:
 						self.__connections[jsonMessage["destination"]].outQueue.append(message)
 
 			elif ((jsonMessage["type"] == "vector")):
-				self.__tableUpdatesQueue.append(message)
+				self.__updateTable(jsonMessage)
 
 			else:
 				printMsgTime(f"{TXT_RED}Testing{TXT_RESET} connection with router {sockStruct.neighbordId} received an unknown message: {message}")
-		return
+		
 
 	def __resendMessages(self, sockStruct):
 		x = 0
 
+	def __updateTable(self, newTable):
+		x = 0
+
+	def __broadcastTable(self):
+		x = 0
+
 	def __shutDown(self):
-		printMsgTime(f'{TXT_RED}|======: Shutting down Router \"{self.__routerID}\":======|{TXT_RESET}')
 		# shutdown the server closes the socket
-		self.__socketServer.close()
+		connectionsList = self.__connections.values()
+		for conn in connectionsList:
+			# print(conn.inQueue)
+			# if (conn.neighbordId != "server"):
+			conn.inQueue.put("threadStop")
+			conn.close()
+
+		printMsgTime(f'{TXT_RED}|======: Router \"{self.__routerID}\" shut down :======|{TXT_RESET}')
 		exit(0)
 
 	def run(self):
 		printMsgTime(f"{TXT_GREEN}|======: Router {self.__routerID} started :======|{TXT_RESET}")
-		self.__connectToServer()
-
+		self.__loadAvailableNode()
+		print(self.__availableNodes)
 		self.__createSockets(self.__connectionsFile)
 		self.__createthreads()
+		self.__connectToServer()
+		self.__runConnectionToServer(self.__connections["server"])
+
 
 	class SocketStruct:
 		def __init__(self, routerIp="-", routerPort="-", otherIp="-", otherPort="-", nId = "-", connectionWeight = 0 ):
 			# this actual router info
 			self.neighbordId = nId
 			self.thisRouterIp = routerIp
-			self.thisRouterPort = routerPort
+			try:
+				self.thisRouterPort = int(routerPort)
+			except:
+				self.thisRouterPort = routerPort
 
 			# neighbord info
 			self.neighbordIp = otherIp
@@ -156,6 +207,7 @@ class Router:
 			# connection info
 			self.weight = connectionWeight
 			self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.sock.setblocking(0) # None blocking socket
 
 			# messages queues
 			self.inQueue = queue.Queue()
@@ -165,7 +217,7 @@ class Router:
 
 
 		def bind(self):
-			self.sock.bind((self.thisRouterPort,self.thisRouterIp))
+			self.sock.bind((self.thisRouterIp, self.thisRouterPort))
 
 		def listen(self):
 			self.sock.listen()
@@ -182,15 +234,12 @@ class Router:
 			self.sock.send(message.encode('UTF-8'))
 
 		def recvMsg(self):
-			# maximum timeout is 5 minutes
-			self.sock.settimeout(0)
 			message = ""
 			try:
 				message = self.sock.recv(4096)
 				printMsgTime(f"{TXT_RED}Testing{TXT_RESET} received: {message}")
 				return message.decode('UTF-8')
-			except socket.timeout:
-				# timout reached
+			except socket.error:
 				return "timeout"
 
 		def connect(self):
@@ -204,7 +253,7 @@ class Router:
 			try:
 				self.sock.connect((self.neighbordIp, self.neighbordPort))
 				return True
-			except socket.error:
+			except socket.error or socket.timeout:
 				return False
 
 		def __connectToServer(self):
@@ -217,7 +266,7 @@ class Router:
 						break
 					except socket.error:
 						printErrors(f"{self.neighbordId} not found. Retrying connection.")
-						sleep(5)
+						sleep(1.5)
 				printMsgTime(f"{TXT_GREEN}Connection established to {self.neighbordId}.{TXT_RESET} ip:{self.neighbordIp} | port:{self.neighbordPort}")
 
 		def close(self):
@@ -226,7 +275,7 @@ class Router:
 
 def main():
 	print(socket.gethostbyname(socket.gethostname()))
-	test =Router("-", "-", "J")
+	test =Router("127.0.0.1", "8081", "J")
 	test.run()
 
 if (__name__ == '__main__'):
