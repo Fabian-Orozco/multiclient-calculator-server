f7adcf16aa83e06a65c985b1eb6b8dc21f05e0b9
Fabian Calvo <fabiianca16@gmail.com>
2022-06-15 18:20:50 -0600

    Packager: convertir las operaciones pequenas en paquetes para ser enviados a los routers

--- src/Packager.py
+++ src/Packager.py
@@ -1,15 +1,15 @@
 from Utilities import *
+from Operation import Package
 
 class Packager:
-    VALID_SYMBOLS = {"(", ")","+", "-", "*", "/", "s", "^"}
-    VALID_OPERATORS = {"+", "-", "*", "/", "^"}
-    ENDLINE = "x"      # Va al inicio de la pila, y en medio de cada operacion o numero enviado a los nodos
+    __VALID_SYMBOLS = {"(", ")","+", "-", "*", "/", "s", "^"}
+    __ENDLINE = "x"      # Va al inicio de la pila, y en medio de cada operacion o numero enviado a los nodos
 
     ## retorna el tope de la pila sin sacarlo
     #
     #  @param pila la pila de la cual se quiere saber el tope
     #  @return el tope de la pila, si el tope es una funcion sqrt, solo retorna "s" para el manejo de la precedencia
-    def inspect(self, pila):
+    def __inspect(self, pila):
         if len(pila) >= 0:
             aux = pila.pop()
             pila.append(aux)
@@ -24,7 +24,7 @@ class Packager:
     #  @param index indice donde se encuentra el "-"
     #  @param string el string con la operacion
     #  @return si el "-" es un signo lo devuelve junto con el numero, si no solo devuelve el "-"
-    def checkMinus(self, index, string):
+    def __checkMinus(self, index, string):
 
         # Operador: 5-5, ()-5, ()-()
         # Signo: -5, (-5), -()
@@ -57,7 +57,7 @@ class Packager:
     #
     #  @param string operacion a la que se le debe agregar espacios
     #  @return operacion con los espacios asignados
-    def insertSpaces(self, string):
+    def __insertSpaces(self, string):
         list = []
         index = 0
         while index < len(string):
@@ -76,7 +76,7 @@ class Packager:
                     index = index + 1
                 current+= string[index]
             elif current == "-":                        # Es un "-"
-                data = self.checkMinus(index,string)
+                data = self.__checkMinus(index,string)
                 current = data[0]
                 index = data[1]
             list.append(current)
@@ -88,7 +88,7 @@ class Packager:
     #
     #  @param operation la operacion infija a transformar, los operandos y los numeros estan separados por espacios
     #  @return un string con la operacion sufija separada por espacios 
-    def infix_to_suffix(self, operation):
+    def __infix_to_suffix(self, operation):
         precedence = {}         # Diccionario que asigna un valor de prioridad a un operador "s" es para la funcion sqrt
         precedence["^"] = 3
         precedence["*"] = 3
@@ -98,12 +98,12 @@ class Packager:
         precedence["("] = 1
         operatorStack = []      # Pila donde almacenamos los operadores
         suffixOperation = []    # Lista donde vamos almacenando la operacion sufija
-        inputSpaced = self.insertSpaces(operation)        # Inserta espacios entre caracteres
+        inputSpaced = self.__insertSpaces(operation)        # Inserta espacios entre caracteres
         print(f"Operacion: {operation}")   
 
         entries = inputSpaced.split() # Lista con las entradas de la operacion (operacion dividida)
         for entry in entries:
-            if (((entry in self.VALID_SYMBOLS) == False)):     # La entrada es un numero
+            if (((entry in self.__VALID_SYMBOLS) == False)):     # La entrada es un numero
                 suffixOperation.append(entry)
             elif entry == "(":                                               # La entrada es un "(", lo agregamos al stack
                 operatorStack.append(entry)
@@ -114,7 +114,7 @@ class Packager:
                     tope = operatorStack.pop()
             else:                                       # Es un operador
                 while(len(operatorStack) != 0) and \
-                    (precedence[self.inspect(operatorStack)] >= precedence[entry]):  # Extraemos los operadores con igual o mayor precedencia, luego ingresamos el operador
+                    (precedence[self.__inspect(operatorStack)] >= precedence[entry]):  # Extraemos los operadores con igual o mayor precedencia, luego ingresamos el operador
                         suffixOperation.append(operatorStack.pop())
                 operatorStack.append(entry)
 
@@ -127,7 +127,7 @@ class Packager:
     #  
     #  @param operation la operacion a evaluar
     #  @return True si el string es operable, False si no
-    def isOperable(self, operation):
+    def __isOperable(self, operation):
         if ((operation[0] == "(") or operation.isnumeric()):
             return True
         else:
@@ -138,16 +138,15 @@ class Packager:
     #  
     #  @param suffix operacion de tipo sufija la cual sera la que se dividira
     #  @return lista de string con las operaciones pequenas
-    def split(self, operation):
+    def __split(self, operation):
         
-        suffixOperation = self.infix_to_suffix(operation)   # convierte la operacion a sufija con espacios
+        suffixOperation = self.__infix_to_suffix(operation)   # convierte la operacion a sufija con espacios
 
-        print(f"Operacion sufija: {suffixOperation}")
         operation = suffixOperation.split()                 # creo lista con las partes de la operacion
         length = len(operation)
 
         stack = []                # Pila para poder dividir la operacion
-        stack.append(self.ENDLINE)
+        stack.append(self.__ENDLINE)
 
         index = 0
         while index < length:       # recorro la operacion
@@ -162,17 +161,17 @@ class Packager:
                     if top2.isnumeric() or top2[0] == "s":  # el segundo tope es un numero
                         data = "(" + top2 + data + top + ")"
                         stack.append(data)
-                        stack.append(self.ENDLINE)
-                    else:                                   # el segundo tope es un ENDLINE
+                        stack.append(self.__ENDLINE)
+                    else:                                   # el segundo tope es un __ENDLINE
                         stack.append(top2)
                         data = data + top
                         stack.append(data)
-                        stack.append(self.ENDLINE)
-                else:                                   # el tope es un ENDLINE
+                        stack.append(self.__ENDLINE)
+                else:                                   # el tope es un __ENDLINE
                     stack.append(top)
                     iStack = len(stack) - 1
 
-                    while ((self.isOperable(stack[iStack]) == False) and (stack[iStack].isnumeric() == False)): # recorro hasta encontrar donde va el operador
+                    while ((self.__isOperable(stack[iStack]) == False) and (stack[iStack].isnumeric() == False)): # recorro hasta encontrar donde va el operador
                         iStack -= 1
                     
                     aux = stack[iStack]
@@ -183,20 +182,41 @@ class Packager:
 
         return stack
 
+
+    def __package(self, listing, id):
+        packages = []
+
+        index = 0
+        part = 0
+
+        while index < len(listing):
+            if listing[index] != "x":
+                package = Package(listing[index], id, part)
+                packages.append(package)
+                part += 1
+            index += 1
+
+        return packages
+
+    def run(self, operation, id):
+        listing = self.__split(operation)
+        print(f"Lista: {listing}")
+        packages = self.__package(listing, id)
+
+        return packages
+
 ## Funcion para correr el programa
 def main():
     packager = Packager()
 
     while True:
         operation = input(" > ingrese la operación: ")
-        
         if operation == "q": break
 
-        listing = packager.split(operation)
+        packages = packager.run(operation, 1)
 
-        print(f"Lista de operaciones: {listing}")
-
-    print(list)
+        for package in packages:
+            print(package.operation)
 
 ## Para ejecutar python3 Packager.py
 #  luego ingrese la operacion
