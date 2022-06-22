7b475337dd1808ea8cd5981a1b94c8ac41e380ee
Marco <marcoandres558@gmail.com>
2022-06-22 15:15:12 -0600

    Implementar algoritmo de paginación : Done

--- src/Calculator.py
+++ src/Calculator.py
@@ -9,6 +9,9 @@ class Calculator:
     def store(self, packet, operation, order):
         self.saver.addOperation(packet, operation, order)
 
-    def calculate(self):
+    def calculate(self,packet, operation, order):
+        self.saver.addOperation(packet, operation, order)
+        self.mmu.updatePagedDisk(self.saver.getElement(self.count)[operation])
         operation = self.mmu.getOperation(self.count)
+        self.count += 1
 
