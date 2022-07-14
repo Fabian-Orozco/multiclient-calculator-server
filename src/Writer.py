28e61f48166dd490c095d1c1a7f3010fdfb0a959
DANIEL ESCOBAR GIRALDO <daniel.escobargiraldo@ucr.ac.cr>
2022-07-13 20:05:13 -0600

    clase que alamacena los resultados en json

---
+++ src/Writer.py
@@ -0,0 +1,51 @@
+import json
+route = "./json/results.json"
+
+# @param newOperation ejemplo: 
+# "{\"index\": 1, \"operation\": \"2+2\", \"result\": \"4\"}"
+def addOperation(newOperation : str, result : str):
+    newOperation = f"{{\"operation\": \"{newOperation}\", \"result\": {result}}}"
+
+    try:
+        # abre archivo y lo almacena como string
+        with open(route) as results: 
+            fie_str = json.dumps(json.load(results))
+    except:
+        results = open(route, "w+")
+        results.write("{\"operations\": []}")
+        results.close()
+        with open(route) as results: 
+            fie_str = json.dumps(json.load(results))
+    if (newOperation not in fie_str):
+        original_str = fie_str
+        # busca en orden inverso del json el primer '}'. No contempla el último chr.
+        offset = fie_str.rindex("]", 0, len(fie_str)-1)
+        # sobreescribe string de 0 a } y agrega coma
+        fie_str = fie_str[:offset:]
+        if ("[]" not in original_str):
+            fie_str += ","
+        # concatena otra operacion al string
+        fie_str = fie_str + newOperation + "]}"
+
+        # sobreescribe el archivo
+        with open(route, "w") as results:
+            results.write(fie_str)
+
+def getOperation(operation : str):
+    resultsFile = open(route, "r")
+    results_json= json.load(resultsFile)
+    for operation_json in results_json["operations"]:
+        if (operation_json["operation"] == operation):
+            return (True, operation_json["result"])
+
+    return (False, operation)
+
+# test code. To run: python Writter.py
+if __name__ == "__main__":
+    print("\nstart\n")
+
+    for k in range(1,5):
+        operation = f"{k}+{k+1}"
+        addOperation(operation, k)
+
+    print("\nend\n")
